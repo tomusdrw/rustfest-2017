f44c7426bb49a49a5e0cda05e6407fce8c1c9e99
Tomasz Drwięga <tomusdrw@gmail.com>
2017-09-28 20:09:27 +0200

    Display decimal ETH balance.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 extern crate web3;
+extern crate bigint;
 
 use std::collections::HashMap;
 
+use bigint::uint::U256;
 use web3::futures::Future;
 use web3::types::Address;
 
@@ -14,9 +16,17 @@ fn main() {
     loop {
         let balance = web3.eth().balance(address.clone(), None).wait().unwrap();
         if let Some(previous) = balances.insert(address.clone(), balance.clone()) {
+            let previous: U256 = (*previous).into();
+            let balance: U256 = (*balance).into();
+
             if previous != balance {
-                println!("New balance for {:?}: {:?}", address, balance);
+                println!("New balance for {:?}: {:?}", address, as_eth(&balance));
             }
         }
     }
+}
+
+fn as_eth(balance: &U256) -> String {
+    let mili_eth = *balance * 1000.into() / U256::from_dec_str("1000000000000000000").unwrap();
+    format!("{:.3} Ether", mili_eth.low_u64() as f64 / 1000f64)
 }
