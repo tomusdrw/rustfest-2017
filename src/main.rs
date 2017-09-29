0d51aa31ccfc143b972c9caeb7114862b2c64da4
Tomasz Drwięga <tomusdrw@gmail.com>
2017-09-29 19:57:00 +0200

    Accessing contract.

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,20 @@
-extern crate bigint;
-extern crate lettre;
 extern crate web3;
 
-use std::collections::HashMap;
-use std::io::BufRead;
-use std::{io, fs, env};
-
-use bigint::uint::U256;
-use lettre::transport::smtp::SmtpTransportBuilder;
-use lettre::transport::EmailTransport;
-use lettre::email::EmailBuilder;
 use web3::futures::Future;
-use web3::types::Address;
-
-fn addresses() -> Vec<Address> {
-    let file = env::args().nth(1).expect("Please provide addresses file.");
-    let file = io::BufReader::new(fs::File::open(file).unwrap());
-    file.lines()
-        .filter_map(|line| {
-            let line = line.unwrap();
-            match line.parse() {
-                Ok(address) => Some(address),
-                Err(err) => {
-                    println!("Ignoring invalid address: {} ({:?})", line, err);
-                    None
-                }
-            }
-        }).collect()
-}
+use web3::contract::{Contract, Options};
+use web3::types::{Address, U256};
 
 fn main() {
-    let (_loop, transport) = web3::transports::Http::new("http://localhost:8545").unwrap();
-    let web3 = web3::Web3::new(transport);
-    let mut balances = HashMap::new();
+    let (_eloop, http) = web3::transports::Http::new("http://localhost:8545").unwrap();
+    let web3 = web3::Web3::new(http);
 
-    let addresses = addresses();
+    // The contract address.
+    let address: Address = "0x00a329c0648769a73afac7f9381e08fb43dbea72".parse().unwrap();
+    // Deploying a contract
+    let contract = Contract::from_json(web3.eth(), address, include_bytes!("./abi.json")).unwrap();
 
-    loop {
-        for address in &addresses {
-            let balance = web3.eth().balance(address.clone(), None).wait().unwrap();
-            if let Some(previous) = balances.insert(address.clone(), balance.clone()) {
-                let previous: U256 = (*previous).into();
-                let balance: U256 = (*balance).into();
-
-                if previous != balance {
-                    println!("New balance for {:?}: {:?}", address, as_eth(&balance));
-                    send_email(&address, &balance);
-                }
-            }
-        }
-    }
-}
-
-
-fn send_email(address: &Address, balance: &U256) {
-    let email = EmailBuilder::new()
-        .from("root@localhost")
-        .to(("tomasz@parity.io", "Tomasz Drwięga"))
-        .subject("ETH Balance Change")
-        .text(&format!("Balance changed for {:?}: {}", address, balance))
-        .build()
-        .unwrap();
-    let mut transport = SmtpTransportBuilder::localhost().unwrap().build();
-    transport.send(email).expect("Should send successfuly.");
-}
-
-fn as_eth(balance: &U256) -> String {
-    let mili_eth = *balance * 1000.into() / U256::from_dec_str("1000000000000000000").unwrap();
-    format!("{:.3} Ether", mili_eth.low_u64() as f64 / 1000f64)
+    // Query the contract instance
+    let result = contract.query("locked", (address, ), None, Options::default(), None);
+    let balance_of: U256 = result.wait().unwrap();
+    assert_eq!(balance_of, 0.into());
 }
